0b306006128ee7842b254d4776334bbd539db354
Aden Lazarus <adenlazarus@Adens-MacBook-Pro.local>
2024-06-23 18:32:19 -0400

    setup course scraper function

--- index.js
+++ index.js
@@ -1,83 +1,123 @@
-const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-console */
+/* eslint-disable no-await-in-loop */
+
+// get all subjects:               https://public.mybustudent.bu.edu/psc/BUPRD/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_COURSE_CATALOG.FieldFormula.IScript_CatalogSubjects?institution=BU001
+// get all courses for a subject: https://public.mybustudent.bu.edu/psc/BUPRD/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_COURSE_CATALOG.FieldFormula.IScript_SubjectCourses?institution=BU001&subject=CASAA
+// get course details for a certain course:https://public.mybustudent.bu.edu/psc/BUPRD/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_COURSE_CATALOG.FieldFormula.IScript_CatalogCourseDetails?course_id=180000
+const axios = require('axios');
 const fs = require('fs');
+const { format } = require('path');
 
+const cookie = 'ExpirePage=https://public.mybustudent.bu.edu/psc/BUPRD/; PS_DEVICEFEATURES=new:1; PS_LASTSITE=https://public.mybustudent.bu.edu/psc/BUPRD/; PS_LOGINLIST=https://public.mybustudent.bu.edu/BUPRD; PS_TOKEN=pQAAAAQDAgEBAAAAvAIAAAAAAAAsAAAABABTaGRyAk4Acwg4AC4AMQAwABR5lgzS43rHR/sTpDo+vD3h4+tuEmUAAAAFAFNkYXRhWXicLYlBDkAwFERfEUsL99BIVVlXsBMJ1g7heg5niP+SN5M/F5CliTHKO+G7MnJwMrDJq3okH1mYKVb9JvZ/9Y4ah6dSBtmJt7dYGjrZC6st0IugjQfiTwyR; PS_TOKENEXPIRE=22_Jun_2024_13:47:20_GMT; PS_TokenSite=https://public.mybustudent.bu.edu/psc/BUPRD/?public-PORTAL-PSJSESSIONID; SignOnDefault=; X-Oracle-BMC-LBS-Route=540f904ca7855914842283d6480e605e7df6ba23e75e63566915a64c6ac704bcdc340990d42672ca8601f87b4028ae8498464c2c05192a70964a6df2; hpt_institution=BU001; lcsrftoken=MB40v5W+vey/ZJFGJN2phPwPK+ACfB3tfJLjWMrnEu4=; public-PORTAL-PSJSESSIONID=x6hAKswlB0F429m-hxQRvuSETp_4fKBr!-158832294';
 
-const scrape = async () => {
-    const allCourses = [];
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    
-    await page.goto('https://www.bu.edu/phpbin/course-search/search.php?page=0&pagesize=10&adv=1&nolog=&search_adv_all=&yearsem_adv=2024-SPRG&credits=*&pathway=&hub_match=all&pagesize=10');
-    while(true) {
-        await page.waitForSelector('.coursearch-results', { timeout: 30000 });
-        
-        const html = await page.content();
-        const $ = cheerio.load(html);
-        const courses = $('.coursearch-results').find('.coursearch-result');
-        
-        courses.each((index, element) => {
-            const heading = $(element).find('.coursearch-result-heading');
-            const description = $(element).find('.coursearch-result-content-description');
-            const hubs = [];
-            $(element).find('.coursearch-result-hub-list').find('li').each((index, element) => {
-                hubs.push($(element).text());
-            });
-            let prereq;
-            
-            try {
-                prereq = $(description).find('p').get(0).children[0].data
-            } catch {
-                prereq = '';
-            }
-            const course = {
-                courseCode: $(heading).find('h6').text(),
-                courseTitle: $(heading).find('h2').text(),
-                prerequisites: prereq,
-                hubs: hubs,
-            }
-            allCourses.push(course);
-            
-        });
-        try {
-            // Click the next button and wait for the next page to load
-            await page.waitForSelector('.coursearch-results-pagination-next', { timeout: 10000 });
-            await new Promise(resolve => setTimeout(resolve, 500));
-            await page.click('.coursearch-results-pagination-next');
-            await page.waitForSelector('.coursearch-results', { timeout: 10000 });
-        } catch (err){
-            break;
-        }
-        
+const numRequests = 50; // number of requests to make at once
+
+async function getCourses() {
+  const allCourses = {};
+  const config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://public.mybustudent.bu.edu/psc/BUPRD/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_COURSE_CATALOG.FieldFormula.IScript_CatalogSubjects?institution=BU001',
+    headers: {
+      Cookie: cookie,
+    },
+  };
+
+  const res = await axios.request(config);
+  const { subjects } = res.data;
+  const subjectCodes = subjects.map((subject) => subject.subject);
+  console.log(subjectCodes);
+
+  const instance = await axios.create({
+    maxBodyLength: Infinity,
+    baseURL: 'https://public.mybustudent.bu.edu/psc/BUPRD/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_COURSE_CATALOG.FieldFormula.IScript_SubjectCourses?institution=BU001',
+    headers: {
+      Cookie: cookie,
+    },
+  });
+
+  for (let i = 0; i < subjects.length; i += numRequests) {
+    const promises = [];
+    for (let j = 0; j < numRequests; j += 1) {
+      console.log(i + j, subjectCodes[i + j]);
+      promises.push(instance.get(`&subject=${subjectCodes[i + j]}`));
     }
-    
-    browser.close();
-    //turn the object into a string and write it to a file in csv format
-    const csv = convertToCSV(allCourses);
-    fs.writeFileSync('courses.csv', csv);
-    console.log('Scraping Complete!');
-    
-    
+    const pRes = await Promise.all(promises);
+    pRes.forEach((courseRes, index) => {
+      const { courses } = courseRes.data;
+      allCourses[subjectCodes[i + index]] = courses;
+    });
+  }
+  fs.writeFileSync('courses.json', JSON.stringify(allCourses));
 }
 
-const convertToCSV = (objArray) => {
-    const array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-    let str = `${Object.keys(array[0]).join(',')}` + '\r\n';
-    
-    for (let i = 0; i < array.length; i++) {
-        let line = '';
-        for (let index in array[i]) {
-            if (line != '') line += ','
-            
-            line += array[i][index];
+async function getCourseDetails() {
+  const details = {};
+  const courses = JSON.parse(await fs.promises.readFile('courses.json'));
+  const instance = await axios.create({
+    maxBodyLength: Infinity,
+    baseURL: 'https://public.mybustudent.bu.edu/psc/BUPRD/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_COURSE_CATALOG.FieldFormula.IScript_CatalogCourseDetails',
+    headers: {
+      Cookie: cookie,
+    },
+  });
+
+  for (let x = 0; x < Object.keys(courses).length; x += 1) {
+    const subject = Object.keys(courses)[x];
+    console.log(subject);
+    console.log(courses[subject].length);
+    for (let i = 0; i < courses[subject].length; i += numRequests) {
+      const promises = [];
+      for (let j = 0; j < numRequests && (i + j) < courses[subject].length; j += 1) {
+        console.log(courses[subject][i + j]);
+        const course = courses[subject][i + j];
+        console.log('COURSE', course);
+        if (course && course.crse_id) {
+          promises.push(instance.get(`?course_id=${course.crse_id}`));
         }
-        
-        str += line + '\r\n';
+      }
+      const res = await Promise.all(promises);
+      res.forEach((courseRes, index) => {
+        console.log(courseRes.data);
+        details[courses[subject[i + index]].crse_id] = courseRes.data;
+      });
+      console.log(details.length);
     }
-    
-    return str;
+  }
+  fs.writeFileSync('details.json', JSON.stringify(courses));
 }
 
-scrape();
+function prepareJSON() {
+  const courses = JSON.parse(fs.readFileSync('courses.json'));
+  const details = JSON.parse(fs.readFileSync('details.json'));
+  const allCourses = {};
+  for (const subject in courses) {
+    allCourses[subject] = [];
+    for (const course of courses[subject]) {
+      allCourses[subject].push({
+        ...course,
+        ...details[course.crse_id],
+      });
+    }
+  }
+  fs.writeFileSync('allCourses.json', JSON.stringify(allCourses));
+}
 
+const formatCourses = () => {
+  const courses = JSON.parse(fs.readFileSync('courses.json'));
+  const formattedCourses = [];
+  for (const subject in courses) {
+    formattedCourses.push({
+      subject,
+      courses: courses[subject],
+    });
+  }
+  fs.writeFileSync('courses.json', JSON.stringify({ subjects: formattedCourses }));
+};
 
+// getCourseDetails();
+// prepareJSON();
+getCourses();
+formatCourses();
